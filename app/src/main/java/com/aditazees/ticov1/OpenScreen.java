2b6510af2c987fb2d233dc215683121cde0bc925
Adit Azees <aditazis@gmail.com>
2019-09-11 19:25:26 +0530

    With SplashScreen

---
+++ app/src/main/java/com/aditazees/ticov1/OpenScreen.java
@@ -0,0 +1,48 @@
+package com.aditazees.ticov1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.WindowManager;
+
+public class OpenScreen extends AppCompatActivity {
+
+    private static int SPLASH_TIME = 1500;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
+        //getActionBar().hide();
+
+        View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+        decorView.setSystemUiVisibility(uiOptions);
+
+        setContentView(R.layout.activity_open_screen);
+
+
+        //Code to start timer and take action after the timer ends
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //Do any action here. Now we are moving to next page
+                Intent mySuperIntent = new Intent(OpenScreen.this, Login.class);
+                startActivity(mySuperIntent);
+                /* This 'finish()' is for exiting the app when back button pressed
+                 *  from Home page which is ActivityHome
+                 */
+                finish();
+            }
+        }, SPLASH_TIME);
+    }
+}
+
